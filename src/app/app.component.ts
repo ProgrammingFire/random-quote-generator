61d4ba62b9b5b1f12b4a9b28a6d49fe5faca2818
ProgrammingFire <nouman.rahman2010@gmail.com>
2021-10-19 21:59:35 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'random-quote-generator';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  quote: any;
+
+  ngOnInit(): void {
+    this.getRandomQuote();
+  }
+
+  getRandomQuote() {
+    this.http
+      .get('https://api.quotable.io/random')
+      .subscribe((data) => (this.quote = data));
+  }
+
+  tweet() {
+    const realText = `${this.quote.content} \nBy ${this.quote.author}`;
+    window.location.href = `https://twitter.com/intent/tweet?text=${realText}`;
+  }
 }
